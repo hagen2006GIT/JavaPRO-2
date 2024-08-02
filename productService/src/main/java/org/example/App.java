e4cae92f5dfb43b213ad2e2e0990e593e30b517a
AKuzminykh <hagen2006-75@yandex.ru>
2024-08-02 12:39:04 +0300

    commit 1 - case 7 удалил комментарии

--- productService/src/main/java/org/example/App.java
+++ productService/src/main/java/org/example/App.java
@@ -1,35 +1,12 @@
 package org.example;
 
-import org.example.repository.ClientRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class App {
 
-    private static ClientRepository clientRepository;
-
-    public App(ClientRepository clientRepository) {
-        App.clientRepository = clientRepository;
-    }
-
-    public static void main( String[] args ) {
-        ApplicationContext ctx = SpringApplication.run(App.class, args);
-
-        clientRepository.findAll().forEach(System.out::println);
-
-//        ProductService productService = ctx.getBean(ProductService.class);
-//        ClientService clientService = ctx.getBean(ClientService.class);
-//
-//        productService.save(new ProductDto(null, "TestAccount_1", 158.8, ProductType.ACCOUNT, 1L));
-//        productService.save(new ProductDto(null, "TestAccount_2", 0.0, ProductType.CARD, 1L));
-//        productService.save(new ProductDto(null, "TestAccount_3", 1058.1, ProductType.CARD, 2L));
-//        productService.save(new ProductDto(null, "TestAccount_4", 34.2, ProductType.ACCOUNT, 3L));
-//        productService.save(new ProductDto(null, "TestAccount_5", 0.0, ProductType.ACCOUNT, 2L));
-//
-//        clientService.save(new ClientDto(null, "TestClient_1"));
-//        clientService.save(new ClientDto(null, "TestClient_2"));
-//        clientService.save(new ClientDto(null, "TestClient_3"));
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
